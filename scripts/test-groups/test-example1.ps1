9ecf60347ebfea3b27fdb2e8ce151c1e31e2c7f2
Elijah Fry <efry@myvelocity.com>
2024-07-25 10:17:09 -0400

    Add millisecond timer to test groups

--- scripts/test-groups/test-example1.ps1
+++ scripts/test-groups/test-example1.ps1
@@ -2,6 +2,14 @@
 
 $repoPath = $(git rev-parse --show-toplevel)
 
+# Start timer
+$startTime = Get-Date
+
 . $repoPath\scripts\test-general-utilities\test-load-env.ps1 -file "$repoPath\scripts\test-general-utilities\sample.env"
 
 . $repoPath\scripts\test-analytics\test-do-api-analytics.ps1
+
+# Print timer in milliseconds
+$endTime = Get-Date
+$elapsedTime = $endTime - $startTime
+Write-Host "Elapsed time: $($elapsedTime.TotalMilliseconds) ms"
